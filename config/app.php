9edba5528a5a4bc37191edd98537dca404047fcc
Felipe Guzman <felipe.lightcone@gmail.com>
2017-12-29 14:33:14 -0600

    installing and configuring laravel socialite

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +226,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        /*
+         * Package Service aliases...
+         */
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
